6213e4cde06d6e580585bf869f8debce59e14b36
gonzalodelpeon <delpg076@newschool.edu>
2017-04-20 17:22:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,8 @@ $(document).ready(function() {
 
 	$("p").css("width", "400px"); 
 
-	$("#photo").animate(
-		{
-			opacity:.3,
-			width: "400px",
-			height: "600px"
-		}, 2000, function() {
-			
-		}
-	);
+	$("#photo").fadeIn("slow", function() {
+		
+	}
+	  );
 });
